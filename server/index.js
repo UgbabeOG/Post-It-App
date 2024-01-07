98bb09609463d0e5dcb9ee40bd1cbe582ee7b55c
UgbabeOG <ugbabe.og@gmail.com>
2024-01-07 04:21:22 +0100

    login api

--- server/index.js
+++ server/index.js
@@ -18,31 +18,49 @@ app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-
 //👇🏻 holds all the existing users this should be converted to database
 const users = [];
 //👇🏻 generates a random string as ID
 const generateID = () => Math.random().toString(36).substring(2, 10);
-
+//register
 app.post("/api/register", async (req, res) => {
-    const { email, password, username } = req.body;
-    const id = generateID();
-    //👇🏻 ensures there is no existing user with the same credentials
-    const result = users.filter(
+  const { email, password, username } = req.body;
+  const id = generateID();
+  //👇🏻 ensures there is no existing user with the same credentials
+  const result = users.filter(
+    (user) => user.email === email && user.password === password
+  );
+  //👇🏻 if true
+  if (result.length === 0) {
+    const newUser = { id, email, password, username };
+    //👇🏻 adds the user to the database (array)
+    users.push(newUser);
+    //👇🏻 returns a success message
+    return res.json({
+      message: "Account created successfully!",
+    });
+  }
+  //👇🏻 if there is an existing user
+  res.json({
+    error_message: "User already exists",
+  });
+});
+//login
+app.post("/api/login", (req, res) => {
+    const { email, password } = req.body;
+    //👇🏻 checks if the user exists
+    let result = users.filter(
         (user) => user.email === email && user.password === password
     );
-    //👇🏻 if true
-    if (result.length === 0) {
-        const newUser = { id, email, password, username };
-        //👇🏻 adds the user to the database (array)
-        users.push(newUser);
-        //👇🏻 returns a success message
+    //👇🏻 if the user doesn't exist
+    if (result.length !== 1) {
         return res.json({
-            message: "Account created successfully!",
+            error_message: "Incorrect credentials",
         });
     }
-    //👇🏻 if there is an existing user
+    //👇🏻 Returns the id if successfuly logged in
     res.json({
-        error_message: "User already exists",
+        message: "Login successfully",
+        id: result[0].id,
     });
 });
