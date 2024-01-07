6686d67d7e11f4ca5850cad91993a7f832bee79a
UgbabeOG <ugbabe.og@gmail.com>
2024-01-07 05:04:05 +0100

    handle like function

--- client/post-it-app/src/utils/Likes.jsx
+++ client/post-it-app/src/utils/Likes.jsx
@@ -1,7 +1,25 @@
 const Likes = ({ numberOfLikes, threadId }) => {
-  const handleLikeFunction = () => {
-    alert("You just liked the post!");
-  };
+    const handleLikeFunction = () => {
+        fetch("http://localhost:4000/api/thread/like", {
+            method: "POST",
+            body: JSON.stringify({
+                threadId,
+                userId: localStorage.getItem("_id"),
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.error_message) {
+                    alert(data.error_message);
+                } else {
+                    alert(data.message);
+                }
+            })
+            .catch((err) => console.error(err));
+    };
 
   return (
     <div className="likes__container">
